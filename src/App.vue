8fd40203265f734a909a79de49bf68065787b1b3
Aragao21 <flaviaaragaolopes@gmail.com>
2023-02-11 05:06:57 -0300

    1.0

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import {ref} from 'vue'
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+let num = ref(0)
+
+</script>
+
+<template>
+  <!-- NAVBAR -->
+  <Navbar/>
+
+  <Footer></Footer>
+</template>
+
+<style>
+body{
+  overflow: hidden;
+  min-height: 100vh;
+  background: rgb(106,196,233);
+  background: linear-gradient(0deg, rgba(106,196,233,0.9360119047619048) 0%, rgba(0,41,150,1) 100%);
+}
+footer{
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  align-items:center;
+  justify-content: center;
+  height: 56px;
+}
+@media (max-width: 600px) {
+  body{
+    overflow: auto;
+  }
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mt-4 text-body-secondary">
+    <div class="card" style="margin-bottom: 100px">
+      <h1 class="text-center mt-3">Pokédex</h1>
+      <div class="card-body">
+        <h2>Objetivo:</h2>
+        <p>Este é um projeto para a etapa 2 do processo seletivo para vaga de desenvolvedor front-end na Copybase!</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
